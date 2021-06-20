4c4d8ec6ec3e7a621498b6631e0c7cc20eab211c
miguelsierramartin <miguel.sierra.martin@alumnos.upm.es>
2021-06-20 18:34:41 +0200

    Monitoreando los mensajes log de postman/papertrair y añadiendo nuevo controlador Service.Catalog #4

---
+++ PlayPadelWeb/src/Services/Catalog/Catalog.Api/Controllers/ProductInStockController.cs
@@ -0,0 +1,51 @@
+﻿using Catalog.Service.EventHandlers.Commands;
+using Catalog.Services.Queries.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service.Common.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("stocks")]
+    public class ProductInStockController : ControllerBase
+    {
+        
+        private readonly ILogger<ProductInStockController> _logger;
+        private readonly IMediator _mediator;
+
+        public ProductInStockController(
+            ILogger<ProductInStockController> logger,
+            IMediator mediator
+        )
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+      /*  public async Task<DataCollection<ProductInStockDto>> GetAll(int page = 1, int take = 10, string products = null)
+        {
+            IEnumerable<int> ids = null;
+
+            if (!string.IsNullOrEmpty(products))
+            {
+                ids = products.Split(',').Select(x => Convert.ToInt32(x));
+            }
+
+            return await _productInStockQueryService.GetAllAsync(page, take, ids);
+        }
+      */
+        [HttpPut]
+        public async Task<IActionResult> UpdateStock(ProductInStockUpdateStockCommand command)
+        {
+            await _mediator.Publish(command);
+            return NoContent();
+        }
+    }
+}
