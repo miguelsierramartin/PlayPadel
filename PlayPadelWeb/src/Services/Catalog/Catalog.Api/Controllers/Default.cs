8ef14277ace3e40e3f00428d128247c2dd19af60
miguelsierramartin <miguel.sierra.martin@alumnos.upm.es>
2021-06-20 13:06:47 +0200

    Configurando la API para Service.Catalog #1

---
+++ PlayPadelWeb/src/Services/Catalog/Catalog.Api/Controllers/Default.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class Default : ControllerBase
+    {
+
+        private readonly ILogger<Default> _logger;
+
+        public Default(ILogger<Default> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Running..";
+        }
+    }
+}
