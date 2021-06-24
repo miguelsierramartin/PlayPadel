42cbb78b5e08e1a08cd3f6fee22207def972d012
miguelsierramartin <miguel.sierra.martin@alumnos.upm.es>
2021-06-24 16:53:01 +0200

    Añadiendo migraciones y modelo para Service.Identity #15

--- PlayPadelWeb/src/Services/Identity/Identity.Api/Startup.cs
+++ PlayPadelWeb/src/Services/Identity/Identity.Api/Startup.cs
@@ -1,7 +1,9 @@
+using Identity.Persistence.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,15 @@ namespace Identity.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbContext
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"),
+                    x => x.MigrationsHistoryTable("__EFMigrationsHistory", "Identity")
+                )
+            );
+
+
             services.AddControllers();
         }
 
