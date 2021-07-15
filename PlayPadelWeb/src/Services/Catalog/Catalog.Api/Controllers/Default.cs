e70b7855ea1906e474d9426a7950ed2898d555b3
miguelsierramartin <miguel.sierra.martin@alumnos.upm.es>
2021-07-15 11:43:48 +0200

    Cambiando controlador Service.Catalog #4

--- PlayPadelWeb/src/Services/Catalog/Catalog.Api/Controllers/Default.cs
+++ PlayPadelWeb/src/Services/Catalog/Catalog.Api/Controllers/Default.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.Service.Queries;
+using Catalog.Services.Queries.DTOs;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Service.Common.Collection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +15,36 @@ namespace Catalog.Api.Controllers
     public class Default : ControllerBase
     {
 
-        private readonly ILogger<Default> _logger;
+        private readonly IProductQueryService _productQueryService;
+        private readonly ILogger<ProductController> _logger;
 
-        public Default(ILogger<Default> logger)
+        public Default(
+            ILogger<ProductController> logger,
+            IProductQueryService productQueryService)
         {
             _logger = logger;
+            _productQueryService = productQueryService;
         }
+
 
         [HttpGet]
         public string Get()
         {
+
             return "Running Catalog ..";
+        }
+
+        [HttpGet]
+        public async Task<DataCollection<ProductDto>> GetAll(int page = 1, int take = 10, string ids = null)
+        {
+            IEnumerable<int> products = null;
+
+            if (!string.IsNullOrEmpty(ids))
+            {
+                products = ids.Split(',').Select(x => Convert.ToInt32(x));
+            }
+
+            return await _productQueryService.GetAllAsync(page, take, products);
         }
     }
 }
